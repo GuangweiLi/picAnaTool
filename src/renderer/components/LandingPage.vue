f7080bc8531f4cb5b792693a251c1dcb13bf4332
liguangwei@geyecloud.com <liguangwei@geyecloud.com>
2020-02-29 16:45:11 +0800

    components

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -19,43 +19,22 @@
       <el-progress v-show="showProcessBar" :text-inside="true" :stroke-width="15" :percentage="percentage"></el-progress>
     </transition>
     <div v-show="result.length && !showProcessBar">
-      <div>
-        <el-pagination
-          :current-page.sync="current"
-          :total="result.length"
-          :page-size="1"
-          :small="true"
-          layout="prev, pager, next">
-        </el-pagination>
-      </div>
-      <h2 class="mgb">
-        <div class="pncInfo">图片名称：{{result.length ? result[current-1].name : ''}}</div>
-        <el-button type="success" icon="el-icon-document-copy" circle title="复制文本" @click="copy"></el-button>
-      </h2>
-      <el-input
-        id="block"
-        type="textarea"
-        :autosize="{ minRows: 10, maxRows: 15}"
-        placeholder="请输入内容"
-        v-model="areaContent">
-      </el-input>
+      <Content :result="result"></Content>
     </div>
   </div>
 </template>
 
 <script>
   import {getText} from '../api/request'
+  import Content from './LandingPage/Content'
 
   export default {
     name: 'landing-page',
-    computed: {
-      areaContent: function() {
-        return this.result.length ? this.result[this.current - 1].content : ''
-      }
+    components: {
+      Content
     },
     data() {
       return {
-        current: 1,
         fileList: [
         ],
         result: [
@@ -65,9 +44,6 @@
       }
     },
     methods: {
-      // open (link) {
-      //   this.$electron.shell.openExternal(link)
-      // },
       // 实现点击复制textarea区域文本内容
       copy() {
         let block = document.getElementById('block');
---
+++ src/renderer/components/LandingPage/Content.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <el-pagination
+        :current-page.sync="current"
+        :total="result.length"
+        :page-size="1"
+        :small="true"
+        layout="prev, pager, next">
+      </el-pagination>
+    </div>
+    <h2 class="mgb">
+      <div class="pncInfo">图片名称：{{result.length ? result[current-1].name : ''}}</div>
+      <el-button type="success" icon="el-icon-document-copy" circle title="复制文本" @click="copy"></el-button>
+    </h2>
+    <el-input
+      id="block"
+      type="textarea"
+      :autosize="{ minRows: 10, maxRows: 15}"
+      placeholder="请输入内容"
+      v-model="areaContent">
+    </el-input>
+  </div>
+</template>
+<script>
+export default {
+  props: ['result'],
+  computed: {
+    areaContent: function() {
+      return this.result.length ? this.result[this.current - 1].content : ''
+    }
+  },
+  data() {
+    return {
+      current: 1
+    }
+  }
+}
+</script>
